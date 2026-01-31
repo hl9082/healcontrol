a60b293044ff74acc68795af3809ba0f63d06a8a
vaibhavgaikwad7 <gaikwadvaibhav114@gmail.com>
2026-01-30 19:37:44 -0500

    Add mock data for emergency demo mode

---
+++ src/mock_data.py
@@ -0,0 +1,7 @@
+MOCK_FAILURE_LOG = """
+test_math.py:12: AssertionError
+__________________________________
+def test_addition():
+>       assert add(2, 2) == 5
+E       AssertionError: assert 4 == 5
+"""
